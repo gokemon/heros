68ff7becf867709923d9022f00de532d88e0c353
gokemon <michael.of.gokey@gmail.com>
2017-03-29 13:15:28 -0400

    setting up app, with grunt

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+  "use strict";
+
+
+  grunt.initConfig({
+    copy: {
+      build: {
+        files: [
+          {
+            expand: true,
+            cwd: "./public",
+            src: ["**"],
+            dest: "./dist/public"
+          },
+          {
+            expand: true,
+            cwd: "./views",
+            src: ["**"],
+            dest: "./dist/views"
+          }
+        ]
+      }
+    },// end of copy
+
+    ts: {
+      app: {
+        files: [{
+          src: ["src/\*\*/\*.ts", "!src/.baseDir.ts"],
+          dest: "./dist"
+        }],
+        options: {
+          module: "commonjs",
+          target: "es6",
+          sourceMap: false
+        }
+      }
+    }, // end of TS conversion
+
+    watch: {
+      ts: {
+        files: ["src/\*\*/\*.ts"],
+        tasks: ["ts"]
+      },
+      views: {
+        files: ["views/**/*.pug"],
+        tasks: ["copy"]
+      }
+    }// watch
+  });//initConfig
+
+
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-ts");
+
+  grunt.registerTask("default", [
+    "copy",
+    "ts"
+  ]);
+
+}; //function(grunt)
